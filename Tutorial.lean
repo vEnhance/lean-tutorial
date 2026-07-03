d5e589f0b238994d6d75679678520d7b5a2c2aea
Evan Chen <evan@evanchen.cc>
2026-07-02 22:24:13 -0500

    feat: hi janabel

---
+++ Tutorial.lean
@@ -0,0 +1,7 @@
+-- This module serves as the root of the `Tutorial` library.
+-- Import modules here that should be built as part of the library.
+import Tutorial.Basic
+
+#check 2+2
+
+theorem hi : 1 = 1 := by rfl
