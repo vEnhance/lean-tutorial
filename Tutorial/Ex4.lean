445f5ce320a0f7f0ff9b1886d0f4d6d931a7a227
janabel <janabeltxia@gmail.com>
2026-07-03 13:27:12 -0500

    update ex4 description

--- Tutorial/Ex4.lean
+++ Tutorial/Ex4.lean
@@ -1,8 +1,15 @@
 import Mathlib.Tactic
+import Mathlib.Combinatorics.Graph.Basic
+import Mathlib.Combinatorics.SimpleGraph.DegreeSum
+import Mathlib.Combinatorics.SimpleGraph.Finite
+set_option warningAsError false
+
 
 /- EXAMPLE 4
 
 In a d-regular simple graph on n vertices, there are n d / 2 edges.
+
+Use mathlib4's SimpleGraph.sum_degrees_eq_twice_card_edges to prove the theorem above
 -/
 
-/- theorem regular_degree_sum : -/
+-- theorem regular_degree_sum ...
